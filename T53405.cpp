e4e95c537c0dafb2474702d33d7e564774121b9f
Douglas <me@micdz.cn>
2018-10-29 21:59:16 +0800

    Create T53405.cpp

---
+++ T53405.cpp
@@ -0,0 +1,24 @@
+//Code by MicDZ
+
+#include<bits/stdc++.h>
+using namespace std;
+#define MAXN 1000000+10
+#define file(a) freopen(a".in","r",stdin);freopen(a".out","w",stdout);
+int read() {
+    int x=0,f=1;
+    char ch=getchar();
+    while(ch>'9'||ch<'0'){if(ch=='-')f=-1;ch=getchar();}
+    while(ch<='9'&&ch>='0'){x=x*10+ch-'0';ch=getchar();}
+    return x*f;
+}
+int a[MAXN];
+int main() {
+    int n=read(),m=read();
+    int sum=0;
+    for(int i=1; i<=n; i++) {
+        a[i]=read();
+        sum+=a[i];
+    }
+    sort(a+1,a+1+n);
+    cout<<(max(a[n],(int)ceil((sum+m)*1.0/n)))<<" "<<a[n]+m<<endl;
+}
